3d69afaed8696e5bbab1cfe94c611ea9d41641af
NEORIS\camilo.rivera <camilo.rivera@neoris.com>
2023-09-28 08:50:50 -0500

    feat: Agregar estructura base del proyecto

---
+++ src/main/java/com/nesum/nesum/NesumApplication.java
@@ -0,0 +1,13 @@
+package com.nesum.nesum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NesumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NesumApplication.class, args);
+	}
+
+}
